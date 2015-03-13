807ade7dcbc61fde90da9b578a7a613d64a568df
Syam Palakurthy <syampalakurthy@gmail.com>
2015-03-12 19:12:18 -0700

    update Git commit including the code the users controller

--- Gemfile
+++ Gemfile
@@ -3,11 +3,13 @@ source 'https://rubygems.org'
 
 
 gem 'rails', '4.2.0'
+gem 'bcrypt', '~>3.1.2'
 gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
+
 
 gem 'turbolinks'
 
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -3,4 +3,12 @@ class TodoList < ActiveRecord::Base
 	validates :title, length: { minimum: 3 }
 	validates :description, length: { minimum: 3 }
 	has_many :todo_items
+
+	def has_completed_items?
+		todo_items.complete.size > 0
+	end
+
+	def has_incompleted_items?
+		todo_items.incomplete.size > 0
+	end
 end
